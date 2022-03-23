a5d7fcbfb67947e67cc06a01b58b0944c49df4c4
liyuanhang <641375508@qq.com>
2022-03-23 22:13:08 +0800

    initial commit

---
+++ src/main/java/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package tree;
+
+public class TreeNode {
+
+    public TreeNode left;
+    public TreeNode right;
+    public int value;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
